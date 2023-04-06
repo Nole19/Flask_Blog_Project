013eaa171a06ccaa2984ed5ed6cb2339e42fe98d
Nole19 <hightech1919@gmail.com>
2023-04-06 14:57:24 +0200

    Debugging + delete post function

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -57,6 +57,6 @@ class UpdateAccountForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired() ])
-    content = TextAreaField("Content", validators=[DataRequired()])
-    sumbit = SubmitField("Post")
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -101,10 +101,10 @@ def new_post():
     return render_template('create_post.html', title='New Post', form=form)
 
 
-@app.route("post/<int:post_id>")
+@app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
-    return render_template("post.html", title=post.title, post=post)
+    return render_template('post.html', title=post.title, post=post)
 
 
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
@@ -125,3 +125,15 @@ def update_post(post_id):
         form.content.data = post.content
     return render_template('create_post.html', title='Update Post',
                            form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
